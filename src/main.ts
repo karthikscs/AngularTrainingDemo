ba532f1e1009607a15447d418a4d13b72ce0fe9d
karthikscs <karthik@simplecodestuffs.com>
2021-02-02 18:55:42 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+/*!
+
+ =========================================================
+ * Simplecodestuffs-training-demo Angular - v1.4.0
+ =========================================================
+
+ * Product Page: https://www.simplecodestuffs.com/product/simplecodestuffs-training-demo-2-angular
+ * Copyright 2020 Simplecodestuffs (https://www.simplecodestuffs.com)
+ * Licensed under MIT (https://github.com/timcreative/simplecodestuffs-training-demo/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
